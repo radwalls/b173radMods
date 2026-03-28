bc0f44595f9f305d9d19f127f6cf4fe8e33e2e6f
radwalls <117958930+radwalls@users.noreply.github.com>
2026-03-28 00:25:38 -0700

    Replace web repel line with climbable rope ladder blocks

--- minecraft_server/src/net/minecraft/src/Block.java
+++ minecraft_server/src/net/minecraft/src/Block.java
@@ -117,6 +117,7 @@ public class Block {
 	public static final Block redstoneRepeaterActive = (new BlockRedstoneRepeater(94, true)).setHardness(0.0F).setLightValue(10.0F / 16.0F).setStepSound(soundWoodFootstep).setBlockName("diode").disableStats().setRequiresSelfNotify();
 	public static final Block lockedChest = (new BlockLockedChest(95)).setHardness(0.0F).setLightValue(1.0F).setStepSound(soundWoodFootstep).setBlockName("lockedchest").setTickOnLoad(true).setRequiresSelfNotify();
 	public static final Block trapdoor = (new BlockTrapDoor(96, Material.wood)).setHardness(3.0F).setStepSound(soundWoodFootstep).setBlockName("trapdoor").disableStats().setRequiresSelfNotify();
+	public static final Block repelRope = (new BlockRepelRope(97, 8)).setHardness(0.2F).setStepSound(soundClothFootstep).setBlockName("repelRope").disableStats().setRequiresSelfNotify();
 	public int blockIndexInTexture;
 	public final int blockID;
 	protected float blockHardness;
