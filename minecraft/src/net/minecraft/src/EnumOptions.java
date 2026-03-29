41620bf44ac6c61bd2d8a39c5c867a8514802815
radwalls <117958930+radwalls@users.noreply.github.com>
2026-03-28 19:11:53 -0700

    Add configurable first-person camera smoothing option

--- minecraft/src/net/minecraft/src/EnumOptions.java
+++ minecraft/src/net/minecraft/src/EnumOptions.java
@@ -5,6 +5,7 @@ public enum EnumOptions {
 	SOUND("options.sound", true, false),
 	INVERT_MOUSE("options.invertMouse", false, true),
 	SENSITIVITY("options.sensitivity", true, false),
+	CAMERA_SMOOTHING("options.cameraSmoothing", true, false),
 	RENDER_DISTANCE("options.renderDistance", false, false),
 	VIEW_BOBBING("options.viewBobbing", false, true),
 	ANAGLYPH("options.anaglyph", false, true),
